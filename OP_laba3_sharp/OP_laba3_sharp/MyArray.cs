49eeacdb54d4c03ea67afbfa65efdd86ccf17b75
AlexTheTrashGolem <a.belorusskij@gmail.com>
2020-03-14 18:40:17 +0200

    Lab3

---
+++ OP_laba3_sharp/OP_laba3_sharp/MyArray.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace OP_laba3_sharp
+{
+    public class MyArray
+    {
+        private int[] _value;
+
+        private int Length
+        {
+            get { return _value.Length; }
+        }
+        
+        
+        public MyArray(int length)
+        {
+            _value = new int[length];
+        }
+        
+        
+
+        public int this[int index]
+        {
+            get
+            {
+                if (index >= Length || index < 0)
+                {
+                    Console.WriteLine("An error occured: Index is out of range.");
+                    return -1;
+                }
+                else
+                {
+                    return _value[index];
+                }
+            }
+
+            set
+            {
+                if (index >= Length || index < 0)
+                {
+                    Console.WriteLine("An error occured: Index is out of range.");
+                }
+                else
+                {
+                    _value[index] = value;
+                }
+            }
+        }
+    }
+}
---
+++ OP_laba3_sharp/OP_laba3_sharp/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace OP_laba3_sharp
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            
+        }
+    }
+}
